bd96ecf2c796e22cc20df7ed4377579a06d5962f
Joachim Schuler <jdh.schuler@gmail.com>
2021-06-04 12:59:48 -0400

    remove chrome package

--- packages/chrome/background.js
+++
@@ -1,9 +0,0 @@
-// this is the background code...
-
-// listen for our browerAction to be clicked
-chrome.browserAction.onClicked.addListener(function (tab) {
-	// for the current tab, inject the "inject.js" file & execute it
-	chrome.tabs.executeScript(tab.id, {
-		file: 'inject.js'
-	});
-});
