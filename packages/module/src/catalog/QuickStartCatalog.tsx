dbcfb8f2a364caa547f2f8f17226e41d7bd8c2fa
Cooper Trowbridge <ctrowbri@redhat.com>
2021-12-22 12:55:40 -0700

    WIP learningpath catalog view

--- packages/module/src/catalog/QuickStartCatalog.tsx
+++ packages/module/src/catalog/QuickStartCatalog.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Gallery, GalleryItem } from '@patternfly/react-core';
+import { Gallery, GalleryItem, Tab, Tabs } from '@patternfly/react-core';
 import { QuickStartContext, QuickStartContextValues } from '../utils/quick-start-context';
 import { QuickStart } from '../utils/quick-start-types';
 import { getQuickStartStatus } from '../utils/quick-start-utils';
@@ -16,25 +16,62 @@ const QuickStartCatalog: React.FC<QuickStartCatalogProps> = ({ quickStarts }) =>
     QuickStartContext,
   );
 
+  const [activeTabKey, setActiveTabKey] = React.useState('lpTab');
+  const handleTabSelect = (_event, tabKey) => {
+    setActiveTabKey(tabKey);
+  };
+
   return (
     <div className="pfext-page-layout__content pfext-is-dark">
-      <Gallery className="pfext-quick-start-catalog__gallery" hasGutter>
-        {quickStarts.map((quickStart) => {
-          const {
-            metadata: { name: id },
-          } = quickStart;
+      <Tabs isBox defaultActiveKey={activeTabKey} onSelect={handleTabSelect}>
+        <Tab title={'Quick Starts'} eventKey={'qsTab'}>
+          <Gallery
+            className="pfext-quick-start-catalog__gallery"
+            hasGutter
+            style={{ paddingTop: '20px' }}
+          >
+            {quickStarts.map((quickStart) => {
+              const {
+                metadata: { name: id },
+              } = quickStart;
 
-          return (
-            <GalleryItem key={id} className="pfext-quick-start-catalog__gallery-item">
-              <QuickStartTile
-                quickStart={quickStart}
-                isActive={id === activeQuickStartID}
-                status={getQuickStartStatus(allQuickStartStates, id)}
-              />
-            </GalleryItem>
-          );
-        })}
-      </Gallery>
+              return (
+                <GalleryItem key={id} className="pfext-quick-start-catalog__gallery-item">
+                  <QuickStartTile
+                    quickStart={quickStart}
+                    isActive={id === activeQuickStartID}
+                    status={getQuickStartStatus(allQuickStartStates, id)}
+                  />
+                </GalleryItem>
+              );
+            })}
+          </Gallery>
+        </Tab>
+        <Tab title={'Learning Paths'} eventKey={'lpTab'}>
+          <Gallery
+            className="pfext-quick-start-catalog__gallery"
+            hasGutter
+            style={{ paddingTop: '20px' }}
+          >
+            {quickStarts.map((quickStart) => {
+              const {
+                metadata: { name: id },
+              } = quickStart;
+
+              return (
+                <GalleryItem key={id} className="pfext-quick-start-catalog__gallery-item">
+                  <QuickStartTile
+                    quickStart={quickStart}
+                    isActive={id === activeQuickStartID}
+                    status={getQuickStartStatus(allQuickStartStates, id)}
+                    isLearningPath
+                  />
+                </GalleryItem>
+              );
+            })}
+          </Gallery>
+        </Tab>
+      </Tabs>
     </div>
   );
 };
