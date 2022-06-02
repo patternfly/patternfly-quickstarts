5ac83d2a41887b7d69454617d9c5f193bbfc17f0
Cooper Trowbridge <ctrowbri@redhat.com>
2022-06-02 16:30:15 -0600

    rm

--- packages/module/docs/patternfly-docs.config.js
+++
@@ -1,8 +0,0 @@
-// This module is shared between NodeJS and babelled ES5
-module.exports = {
-  sideNavItems: [
-    { section: 'extensions' },
-  ],
-  topNavItems: [],
-  port: 8003,
-};
--- packages/module/docs/src/content/index.md
+++
@@ -1,4 +0,0 @@
----
-id: Quick starts
-section: extensions
----
