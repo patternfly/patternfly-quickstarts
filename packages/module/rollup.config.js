81a7f58cedb9eaf53d78876de03b90f3c8909b49
Cooper Trowbridge <ctrowbri@redhat.com>
2022-06-06 13:55:26 -0600

    lint

--- packages/module/rollup.config.js
+++ packages/module/rollup.config.js
@@ -36,7 +36,7 @@ const cjsBuild = {
     file: packageJson.main,
     format: 'cjs',
     sourcemap: true,
-    inlineDynamicImports: true
+    inlineDynamicImports: true,
   },
   external: ['react', 'react-dom'],
   plugins: [
