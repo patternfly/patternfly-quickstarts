b96ecd02f97923e7383560e8e15011646009c7c5
Nicole Thoen <nthoen@redhat.com>
2023-03-21 09:08:05 -0400

    fix(release.config): add commas

--- packages/module/release.config.js
+++ packages/module/release.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   branches: [{ name: 'main', channel: 'prerelease' }],
   analyzeCommits: {
-    preset: 'angular'
+    preset: 'angular',
   },
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/github',
-    '@semantic-release/npm'
+    '@semantic-release/npm',
   ],
-  tagFormat: 'v${version}'
+  tagFormat: 'v${version}',
 };
