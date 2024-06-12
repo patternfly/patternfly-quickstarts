cd06d8ec1a5364fa3e506fb3020624865e91689d
Nicole Thoen <nthoen@redhat.com>
2024-06-12 09:45:20 -0400

    fix: Update release.config.js to publish alphas from v6

--- packages/module/release.config.js
+++ packages/module/release.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   branches: [
     'do-not-delete',
-    { name: 'main', channel: 'alpha', prerelease: 'alpha' },
-    { name: 'v6', channel: 'prerelease-v6', range: '6.x' },
+    { name: 'main', channel: 'prerelease', prerelease: 'prerelease' },
+    { name: 'v6', channel: 'alpha', prerelease: 'alpha' }
   ],
   analyzeCommits: {
     preset: 'angular',
