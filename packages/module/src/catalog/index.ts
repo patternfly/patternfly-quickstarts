341fc4585574289dc0774f6e024cda81d8a4696b
Nicole Thoen <nthoen@redhat.com>
2024-07-30 16:36:54 -0400

    fix: update to PF6 betas + some style tweaks (#317)
    * fix: update to PF6 betas + some style tweaks
    
    * fix demo masthead layout

--- packages/module/src/catalog/index.ts
+++ packages/module/src/catalog/index.ts
@@ -4,6 +4,6 @@ export { default as QuickStartTile } from './QuickStartTile';
 export { default as QuickStartTileDescription } from './QuickStartTileDescription';
 export { default as QuickStartTileFooter } from './QuickStartTileFooter';
 export { default as QuickStartTileFooterExternal } from './QuickStartTileFooterExternal';
-export { default as QuickStartTileHeader, QuickstartAction } from './QuickStartTileHeader';
+export { default as QuickStartTileHeader } from './QuickStartTileHeader';
 export * from './Toolbar/QuickStartCatalogFilterItems';
 export * from './Catalog';
