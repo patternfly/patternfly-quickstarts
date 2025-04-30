149f52ce896531b19bb284ee79460dbcca0cc3f1
Katie McFaul <kmcfaul@redhat.com>
2025-04-30 12:26:14 -0400

    fix app test

--- packages/dev/src/App.test.tsx
+++ packages/dev/src/App.test.tsx
@@ -1,10 +1,9 @@
-import { createRoot } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './AppContext';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   const root = createRoot(div);
   root.render(<App />);
-  const root = createRoot(div);
   root.unmount();
 });
